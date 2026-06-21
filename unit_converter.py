79a9b2457ca681ead46e45ce3eac7f2f077e35ad
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-21 10:54:12 +0530

    added

---
+++ unit_converter.py
@@ -0,0 +1,9 @@
+# Simple Unit Converter
+
+print("UNIT CONVERTER")
+print("1. Kilometers to Miles")
+print("2. Celsius to Fahrenheit")
+print("3. Kilograms to Pounds")
+
+choice = int(input("Enter your choice (1-3): "))
+
