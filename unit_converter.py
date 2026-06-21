d5033be1848bd0a4328554d6d215a3523a0a098e
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-21 10:56:11 +0530

    celsius to fahrenheit

--- unit_converter.py
+++ unit_converter.py
@@ -11,3 +11,9 @@ if choice == 1:
     km = float(input("Enter distance in kilometers: "))
     miles = km * 0.621371
     print("Miles:", round(miles, 2))
+
+elif choice == 2:
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print("Fahrenheit:", round(fahrenheit, 2))
+
